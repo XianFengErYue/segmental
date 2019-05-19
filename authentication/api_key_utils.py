5178c695cfb2c6181cfef2eb2308a0945e5447d3
sachsbl <sachsbl@gmail.com>
2019-05-19 15:26:18 -0600

    adding simple api key auth

--- authentication/api_key_utils.py
+++ authentication/api_key_utils.py
@@ -1,10 +1,11 @@
 import boto3
 
 
-def get_api_key_from_s3(bucket='segmental', key='api_key.txt'):
+# assumes one line per file containing only the api key
+def get_api_key_from_s3(bucket='segmental', key_file='api_key.txt'):
     s3 = boto3.resource('s3')
 
-    obj = s3.Object(bucket, key)
+    obj = s3.Object(bucket, key_file)
     key = obj.get()['Body'].read().decode('utf-8')
 
     return key
